98209c25f92e579ddfaa49bb1ce590dd4c865d8a
olivervillalobos <olivervb999@gmail.com>
2024-02-29 18:18:45 -0800

    Se agrego un media querie del index y una funcionalidad a boton de enviar en la seccion de contacto

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
-var myVar;
-            
-function myFunction() {
-  myVar = setTimeout(showPage, 2000);
-}
+let nombre = document.querySelector("#nombre");
+let telefono = document.querySelector("#telefono");
+let fecha = document.querySelector("#fecha");
+let comentarios = document.querySelector("#comentarios");
+let boton_forma = document.querySelector("#boton_forma");
 
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
+boton_forma.disabled = true; //setting button state to disabled
+
+nombre.addEventListener("change", stateHandle);
+telefono.addEventListener("change", stateHandle);
+fecha.addEventListener("change", stateHandle);
+comentarios.addEventListener("change", stateHandle);
+
+function stateHandle() {
+    if (document.querySelector("#nombre" && "#telefono" && "#fecha" && "#comentarios").value === "") {
+        boton_forma.disabled = true; //button remains disabled
+    } else {
+        boton_forma.disabled = false; //button is enabled
+    }
 }
